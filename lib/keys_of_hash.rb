6c7be81e980e59779bc7b61eb96d2477142311e8
Otha D Hernandez <otha.hernandez@gmail.com>
2019-05-03 02:37:13 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,24 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+  t = []
+  t << arguments
+  arr = []
+    h = {
+    "sugar glider"=>"Australia",
+    "aye-aye"=>"Madagascar",
+    "red-footed tortoise"=>"Panama",
+    "kangaroo"=>"Australia",
+    "tomato frog"=>"Madagascar",
+    "koala"=>"Australia"
+  }
   end
 end
+
+# {"sugar glider"=>"Australia",
+# "aye-aye"=>"Madagascar",
+# "red-footed tortoise"=>"Panama",
+# "kangaroo"=>"Australia",
+# "tomato frog"=>"Madagascar",
+# "koala"=>"Australia"}
