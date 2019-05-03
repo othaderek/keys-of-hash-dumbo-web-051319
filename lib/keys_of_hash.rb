7110a45f686d3deb2bfb9ea8e3177f6f23149955
Otha D Hernandez <otha.hernandez@gmail.com>
2019-05-03 02:54:54 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,27 +1,27 @@
 require 'pry'
 
 class Hash
-def find_key(*arg)
-  t = []
-  t = arg
-  arr = []
-    h = {
-    "sugar glider"=>"Australia",
-    "aye-aye"=>"Madagascar",
-    "red-footed tortoise"=>"Panama",
-    "kangaroo"=>"Australia",
-    "tomato frog"=>"Madagascar",
-    "koala"=>"Australia"
-  }
-
-  t.each do |i|
-    h.each do |k, v|
-      if i == h[k]
-        arr << k
+  def find_key(*arg)
+    t = []
+    t = arg
+    arr = []
+      h = {
+      "sugar glider"=>"Australia",
+      "aye-aye"=>"Madagascar",
+      "red-footed tortoise"=>"Panama",
+      "kangaroo"=>"Australia",
+      "tomato frog"=>"Madagascar",
+      "koala"=>"Australia"
+    }
+  
+    t.each do |i|
+      h.each do |k, v|
+        if i == h[k]
+          arr << k
+        end
       end
     end
   end
-end
 end
 
 # {"sugar glider"=>"Australia",
